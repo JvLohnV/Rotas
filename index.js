caa6ea8179f134043059d781a533fbf1d4d7b958
João Vitor Lohn <89606838+JvLohnV@users.noreply.github.com>
2024-09-23 17:03:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const rotas = require('./rotas/rotas_produtos')
-const clientes = require('./rotas/rotas_clientes')
-const rotas_autenticacao = require("./rotas/rotas_autenticacao")
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const rotasProdutos = require('./rotas/rotas_produtos');
+const rotasClientes = require('./rotas/rotas_clientes');
+const rotasAutenticacao = require('./rotas/rotas_autenticacao');
 
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use('/produtos', rotas)
-app.use('/clientes', clientes)
-app.use('/auth', rotas_autenticacao)
+const app = express();
+const swaggerDocument = YAML.load('./docs/documentacao.yaml');
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Rotas
+app.use('/produtos', rotasProdutos);
+app.use('/clientes', rotasClientes);
+app.use('/auth', rotasAutenticacao);
+
+// Swagger Docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8507) 
 
